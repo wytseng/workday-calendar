9ba55b1f640e80861edd57837987460b843d9f07
Steph T <wytseng@uci.edu>
2022-11-18 11:20:49 -0800

    add js code to save event in local storage

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  var saveBtn = $('.saveBtn');
+  $('.saveBtn').on('click', function() {
+    let hourClickedEl = $(this).parent();
+    let hourClicked = hourClickedEl.attr('id');
+    
+    let hourDesc = hourClickedEl.children('.description');
+    if ($.trim(hourDesc.val())) {
+      let eventDesc = $.trim(hourDesc.val());
+      localStorage.setItem(hourClicked, eventDesc);
+    } else {
+      console.log('input empty');
+    }    
+  }) 
 
   //
   // TODO: Add code to apply the past, present, or future class to each time
@@ -23,5 +36,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  // $('#currentDay').text(currentTime.format('dddd, MMMM D, YYYY'));
 
 });
